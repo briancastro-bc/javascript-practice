d6561e73d84f128360af8849283c572d7222e8d9
briancastro-bc <brian.castro.bedoya@gmail.com>
2024-04-23 17:40:57 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,69 @@
+// alert("Adios mundo");
+
+// Comentarios en javascript
+// Comentarios de una sola linea
+/**
+ * 
+ * Comentarios
+ * de 
+ * multiples lineas
+ * 
+ */
+
+/**
+ * 
+ * Variables: una variable es un espacio en la memoria
+ * del ordenador que podrá cambiar durante la ejecución 
+ * del programa
+ * 
+ * sintaxis: var/let nombreVariable = valor_variable
+ * 
+ * 
+ * NO PUEDE TENER: espacios, no puede iniciar 
+ * con numeros, no puede caracteres especiales
+ */
+
+// Forma numero 1 de declarar variables
+// var némbreDeMiVariable = "hola mundo";
+// // !DO
+// var camelCase = "camelCase";
+// var snake_case = "snake_case";
+// var $foo = "foo";
+
+// // !DON'T DO
+// // var 1var = "my var";
+// // var varíable = "mi variable";
+
+// // !CASE SENSITIVE
+// var miNombreDeVariable = 5;
+// var minombredevariable = 10;
+// var MiNombreDeVariable = 5;
+// var miNombredeVariable = 5;
+
+// camelCase = 10;
+
+// // var bar;
+// // bar = "foo";
+
+// var foo = "bar";
+
+// let bar = "hola";
+
+// alert(bar);
+let numberOne = 10;
+let numberTwo = 20;
+
+let result = parseInt(numberOne + numberTwo) / 2;
+
+document.write(`El resultado de la operacion es: ${result}`);
+
+
+
+
+
+
+
+
+
+
+
